84e824c6ec65e2c83899e95f8fbb42cf032a49fb
deguzg <deguzgb@gmail.com>
2022-03-10 20:39:40 -0600

    finished bio but need to update text

---
+++ gillian.js
@@ -0,0 +1,11 @@
+document.querySelectorAll(".section").style.backgroundColor = "purple";
+
+const nodeList = document.querySelectorAll(".section");
+console.log(nodeList)
+for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].style.backgroundColor = "purple";
+}
+
+function revealFact() {
+    
+}
