c611edf4cf33dfd4e40b5501b9638117f9d0e2bc
Sarah <vanwars@gmail.com>
2022-05-23 07:13:42 -0500

    updating accessibility of modals

--- js/modal/modal.js
+++ js/modal/modal.js
@@ -4,27 +4,37 @@ function createModal(container) {
     const closeSelector = container + ' .close';
     // Get the modal
     var modal = document.querySelector(modalSelector);
+    modal.setAttribute('aria-hidden', 'true');
 
     // Get the button that opens the modal
     var btns = document.querySelectorAll(buttonSelector);
 
     // Get the <span> element that closes the modal
-    var span = document.querySelector(closeSelector);
+    var closeButton = document.querySelector(closeSelector);
+    closeButton.setAttribute('aria-hidden', 'true');
 
     // When the user clicks on the button, open the modal
-    btns.forEach(btn => {
-        btn.onclick = function() {
+    btns.forEach((btn, idx) => {
+        btn.id = `${container}-open-modal-${idx}`;
+        console.log(btn.id);
+        btn.onclick = ev => {
             modal.classList.add('show');
             modal.classList.remove('hide');
-            // modal.style.display = "block";
+            modal.querySelector('.close').setAttribute('data-button-id', ev.currentTarget.id);
+            modal.setAttribute('aria-hidden', 'false');
+            modal.querySelector('.close').focus();
         }
     });
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
+    closeButton.onclick = function (ev) {
+        console.log(ev.currentTarget);
+        ev.currentTarget.setAttribute('aria-hidden', 'true');
         modal.classList.remove('show');
         modal.classList.add('hide');
-        // modal.style.display = "none";
+        modal.setAttribute('aria-hidden', 'true');
+        const sourceButtonId = ev.currentTarget.dataset.buttonId;
+        document.getElementById(sourceButtonId).focus();
     }
 
     // When the user clicks anywhere outside of the modal, close it
@@ -33,7 +43,7 @@ function createModal(container) {
             modal.style.display = "none";
         }
     });
-}
+};
 
 createModal('#speaking');
 createModal('.guiding-principles');
